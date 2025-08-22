6401e0dd614985a5715e3776178a55621e6564ad
Abdelrahman <rafeeq220044@gmail.com>
2025-08-23 02:21:20 +0300

    Refactor Todo App: Implement core functionality, enhance styling, and remove unused assets

--- src/App.js
+++ src/App.js
@@ -1,23 +1,65 @@
-import logo from './logo.svg';
-import './App.css';
+import { useRef, useState, useEffect } from "react";
+import "./App.css";
 
 function App() {
+  const [todos, setTodos] = useState(() => {
+    const savedTodos = localStorage.getItem("todos");
+    return savedTodos
+      ? JSON.parse(savedTodos)
+      : [
+          { text: "Learn React", completed: false },
+          { text: "Build a Todo App", completed: false },
+          { text: "Testing", completed: false },
+          { text: "Deployment", completed: false },
+        ];
+  });
+  const inputRef = useRef();
+  const addTodo = function () {
+    const text = inputRef.current.value;
+    if (text === "") return;
+    const newItem = { text, completed: false };
+    setTodos([...todos, newItem]);
+    inputRef.current.value = "";
+    inputRef.current.focus();
+  };
+  const toggleTodo = function (index) {
+    const newTodos = [...todos];
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
+  const deleteTodo = function (index) {
+    const newTodos = todos.filter((_, i) => i !== index);
+    setTodos(newTodos);
+  };
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="container">
+        <h2>Todo List</h2>
+        <ul>
+          {todos.map(({ text, completed }, index) => {
+            return (
+              <li className={completed ? "done" : ""} key={index}>
+                <span
+                  className="circle"
+                  onClick={() => toggleTodo(index)}
+                ></span>
+                <span className="text" onClick={() => toggleTodo(index)}>
+                  {text}
+                </span>
+                <span
+                  className="delete"
+                  onClick={() => deleteTodo(index)}
+                ></span>
+              </li>
+            );
+          })}
+        </ul>
+        <input ref={inputRef} placeholder="Enter an item..."></input>
+        <button onClick={addTodo}>Add Task</button>
+      </div>
     </div>
   );
 }
